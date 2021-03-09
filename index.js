6d4fb35ef6b0e586b5e3e686016daa2974341230
RehimRay <rhmcml@gmail.ru>
2021-03-09 22:47:41 +0400

    Add the main function to index. make the function async

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const {customerDatabase, sellerDatabase, productDatabase} = require ('./database')
 const printSelectHistory = require('./lib/printSelectHistory')
 
-
-const pasha = customerDatabase.findBy('name', 'Kenan')
- 
-printSelectHistory(pasha)
-
+    async function main() {
+        const kenan = await customerDatabase.findBy('name', 'Kenan')
+        printSelectHistory(kenan)
+    }
+    
+main()
