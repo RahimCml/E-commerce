3f64771604734efdf26197dd129ea42b6957de7a
RehimRay <rhmcml@gmail.ru>
2021-03-27 00:38:32 +0400

    Create index.pug and prodcut.pug inside the views folder. Print product.pug product extension

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require("express");
-const { customerDatabase } = require("./database");
-const flatted = require("flatted");
+const express = require('express')
+const ProductDatabase = require('./database/product-database')
+const flatted = require('flatted')
 
-const app = express();
+const app = express()
 
-app.get("/customers", async (req, res) => {
-	const customers = await customerDatabase.load();
-	res.send(flatted.stringify(customers));
-});
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/product', async (req, res) => {
+  const product = await ProductDatabase.load()
+  res.render('product', { product })
+  //   res.send(flatted.stringify(prodcut))
+})
 
 app.listen(3000, () => {
-	console.log("started listening on 3000");
-});
+  console.log('started listening on 3000')
+})
