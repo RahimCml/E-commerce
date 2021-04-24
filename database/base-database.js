cbc4497c8544fab939d144110b19f7c3199aeefc
RehimRay <rhmcml@gmail.ru>
2021-04-24 16:22:00 +0400

    Create the removeBy function

--- database/base-database.js
+++ database/base-database.js
@@ -47,6 +47,20 @@ class BaseDatabase {
     this.save(objects)
   }
 
+  async removeBy(property, value) {
+    const objects = await this.load()
+
+    const index = objects.findIndex((o) => o[property] == value)
+
+    if (index == -1)
+      throw new Error(
+        `Cannot find ${this.model.name} instance with ${property} ${object.id}`,
+      )
+
+    objects.splice(index, 1)
+    this.save(objects)
+  }
+
   async update(object) {
     const objects = await this.load()
 
