4b7d22d3ccab118c43f4f7803c7fb5bcacc862ac
RehimRay <rhmcml@gmail.ru>
2021-04-26 23:26:55 +0400

    Transfer main index.js to index.js in routes. Assign as router.get

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const ProductsRouter = require('./routes/products')
+const productsRouter = require('./routes/products')
 
 const app = express()
 app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 
-app.use('/products', ProductsRouter)
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use('/products', productsRouter)
 
 app.listen(3000, () => {
   console.log('started listening on 3000')
