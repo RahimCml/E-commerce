53c81e6187d1f657b2eff2cac3f221bc03ec48c7
RehimRay <rhmcml@gmail.ru>
2021-04-29 00:24:08 +0400

    Install Mongodb. add a new file. import mongoose to file

---
+++ mongo-connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/e-commerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('we are connected to mongodb')
+})
