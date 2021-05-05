4e25d418e1674ec379a691a1fd2786c3bb91872e
RehimRay <rhmcml@gmail.ru>
2021-05-05 14:52:21 +0400

    Import seller Database and load it

--- routes/seller.js
+++ routes/seller.js
@@ -1,5 +1,9 @@
+const { sellerDatabase } = require('../database')
+
 const router = require('express').Router()
 
-router.get('/', async (req, res) => {})
+router.get('/', async (req, res) => {
+  const sellers = await sellerDatabase.load()
+})
 
 module.exports = router
