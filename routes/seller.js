5e3faf31c295262309deb15d9b91d36329210e6a
RehimRay <rhmcml@gmail.ru>
2021-05-05 14:56:39 +0400

    Insert sellerDatabase in post method

--- routes/seller.js
+++ routes/seller.js
@@ -8,4 +8,10 @@ router.get('/', async (req, res) => {
   res.render('sellers', { sellers })
 })
 
+router.post('/', async (req, res) => {
+  const seller = await sellerDatabase.insert(req.body)
+
+  res.send(seller)
+})
+
 module.exports = router
