32ddc5449845f0ce4d9c97229733ccb8c60899bf
RehimRay <rhmcml@gmail.ru>
2021-05-08 02:06:54 +0400

    Assign Seller as an array and reference Seller in product.js

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  seller: { type: String, required: true },
+  seller: [{ type: String, ref: 'Seller', required: true }],
   size: { type: String, required: true },
   customer: { type: String },
   price: { type: Number, required: true },
