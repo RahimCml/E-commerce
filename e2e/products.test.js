bc640c32404c65f9c2476db7fe70e5e39482a75b
RehimRay <rhmcml@gmail.ru>
2021-05-13 15:52:37 +0400

    Do send productToCreate

--- e2e/products.test.js
+++ e2e/products.test.js
@@ -9,7 +9,7 @@ test('creates a new product', async (done) => {
     price: 20,
   }
 
-  const response = await request.post('/products')
+  const response = await request.post('/products').send(productToCreate)
 
   done()
 })
