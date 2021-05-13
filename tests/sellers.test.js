ca6c3114e533bcc584bfdc5c0184ce069f5a6f7d
RehimRay <rhmcml@gmail.ru>
2021-05-13 16:33:29 +0400

    Create a variable named sellerToCreate and add name product to it.

--- tests/sellers.test.js
+++ tests/sellers.test.js
@@ -2,5 +2,10 @@ const app = require('../')
 const request = require('supertest')(app)
 
 test('creates a new seller', async (done) => {
+  const sellerToCreate = {
+    name: 'Chanel',
+    product: 'Jeans',
+  }
+
   done()
 })
