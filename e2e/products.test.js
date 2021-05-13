525db2074c6289e15fee34729ff476ad2b495cf4
RehimRay <rhmcml@gmail.ru>
2021-05-13 15:51:16 +0400

    Create a variable named productToCreate add name, seller, size and price to it.

--- e2e/products.test.js
+++ e2e/products.test.js
@@ -2,5 +2,12 @@ const app = require('../')
 const request = require('supertest')(app)
 
 test('creates a new product', async (done) => {
+  const productToCreate = {
+    name: 'Jeans',
+    seller: ['Chanel'],
+    size: 'l',
+    price: 20,
+  }
+
   done()
 })
