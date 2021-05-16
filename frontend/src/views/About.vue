4cbbfa395fb004a6bf55f6ffab0f2b424964c15f
RehimRay <rhmcml@gmail.ru>
2021-05-17 02:38:53 +0400

    Add initial frontend files

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,17 @@
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'About',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+<template lang="pug">
+.about
+  h1 This is an about page
+  HelloWorld(msg="Hello guys !!")
+</template>
