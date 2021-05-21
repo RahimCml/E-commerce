7730a1e9244c0680c2ca7a18bb4ab11146203a00
RehimRay <rhmcml@gmail.ru>
2021-05-21 18:37:24 +0400

    To test the sellers, create a sellers.test

---
+++ tests/e2e/sellers.test.js
@@ -0,0 +1,20 @@
+const app = require('../..')
+const request = require('supertest')(app)
+
+test('creates a new seller', async (done) => {
+  const sellerToCreate = {
+    name: 'Chanel',
+    product: 'Jeans'
+  }
+
+  const response = await request
+    .post('/sellers')
+    .send(sellerToCreate)
+    .expect(200)
+
+  const sellerCreated = response.body
+
+  expect(sellerCreated).toMatchObject(sellerToCreate)
+
+  done()
+})
