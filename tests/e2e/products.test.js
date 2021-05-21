a0020251633067172ffb95c1137e3604d5b7cbf9
RehimRay <rhmcml@gmail.ru>
2021-05-21 18:36:28 +0400

    To test the products, create a products.test

---
+++ tests/e2e/products.test.js
@@ -0,0 +1,22 @@
+const app = require('../..')
+const request = require('supertest')(app)
+
+test('creates a new product', async (done) => {
+  const productToCreate = {
+    name: 'Jeans',
+    seller: ['Chanel'],
+    size: 'L',
+    price: 20,
+  }
+
+  const response = await request
+    .post('/products')
+    .send(productToCreate)
+    .expect(200)
+
+  const productCreated = response.body
+
+  expect(productCreated).toMatchObject(productToCreate)
+
+  done()
+})
