8ff284b1a722de04bf8dcca1653a5a091c0bcd10
RehimRay <rhmcml@gmail.ru>
2021-05-21 15:54:03 +0400

    Generate states and mutations for subtraction and increment

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,30 @@
+import axios from 'axios';
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const Mutations = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT'
+}
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		countHome: 0,
+		countAbout: 0
+	},
+	mutations: {
+		[Mutations.INCREMENT](type, state) {
+			type === "countHome" ? state.countHome++ : state.countAbout++;
+		},
+		[Mutations.INCREMENT](type, state) {
+			if (state[type] === 0) return;
+
+			type === "countHome" ? state.countHome-- : state.countAbout--;
+		},
+	},
+	actions: {
+	},
+	modules: {},
+});
