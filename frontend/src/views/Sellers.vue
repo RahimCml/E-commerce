6b661dec9fb4a932194aa0618d9c9478ff3488ac
RehimRay <rhmcml@gmail.ru>
2021-05-23 01:54:00 +0400

    Call fetchSellers in methods and assign fetchSellers to sellers

---
+++ frontend/src/views/Sellers.vue
@@ -0,0 +1,24 @@
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'Sellers',
+  data () {
+    return {
+      isLoading: true,
+      sellers: []
+    }
+  },
+  async created () {
+    this.sellers = await this.fetchSellers()
+  },
+  methods: {
+    ...mapActions(['fetchSellers'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Sellers
+</template>
