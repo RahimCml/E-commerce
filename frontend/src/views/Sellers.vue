d2e91b46b5808a33a65eaf69533b52f5b6b65224
RehimRay <rhmcml@gmail.ru>
2021-05-23 01:54:53 +0400

    Set isLoading false and print sellers information in pug

--- frontend/src/views/Sellers.vue
+++ frontend/src/views/Sellers.vue
@@ -11,6 +11,7 @@ export default {
   },
   async created () {
     this.sellers = await this.fetchSellers()
+    this.isLoading = false
   },
   methods: {
     ...mapActions(['fetchSellers'])
@@ -21,4 +22,10 @@ export default {
 <template lang="pug">
   .home
     h1 Sellers
+        p(v-if="isLoading") Please wait...
+    div(v-else)
+      p There are {{sellers.length}} sellers for sale.
+      ol
+        li(v-for="seller in sellers")
+          a(:href="`/sellers/${seller._id}`") {{ seller.name }}
 </template>
