364b3098839142af505911019b22ba7007d1c4d2
RehimRay <rhmcml@gmail.ru>
2021-05-23 00:31:51 +0400

    Add a variable and a state do decrement and increment using mutations

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const Mutations = {
+	INCREMENT: "INCREMENT",
+	DECREMENT: "DECREMENT",
+};
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		countAbout: 0
+	},
+	mutations: {
+		[Mutations.INCREMENT](state, type) {
+			type === "countHome" ? 0 : state.countAbout++;
+		},
+		[Mutations.DECREMENT](state, type) {
+			if (state[type] === 0) return;
+
+			type === "countHome" ? 0 : state.countAbout--;
+		},
+	},
+	actions: {
+		increment({ commit }, type) {
+			commit(Mutations.INCREMENT, type);
+		},
+		decrement({ commit }, type) {
+			commit(Mutations.DECREMENT, type);
+		},
+	},
+});
