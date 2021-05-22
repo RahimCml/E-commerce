402ac8ba58ae00eb1e2d1188698d3b9fe0f369a2
RehimRay <rhmcml@gmail.ru>
2021-05-22 23:55:37 +0400

    Edit customer model as Schema

--- models/customer.js
+++ models/customer.js
@@ -1,24 +1,18 @@
+const mongoose = require("mongoose");
 const Order = require('./order')
-const uuid = require('uuid')
 
-class Customer {
-  constructor(id = uuid.v4(), name, address, orders = [], basket = []) {
-    this.id = id
-    this.name = name
-    this.address = address // seller'in customer addressini gormesini asgla
-    this.basket = basket
-    this.orders = orders
-  }
-  buyProduct(products, seller) {
-    const selectedOrder = new Order(products, seller, this)
+const CustomerSchema = new mongoose.Schema({
+	name: { type: String, required: true, minlength: 2 },
+	address: { type: String, required: true },
+  	email: { type: String, required: true },
+	orders: [
+		{
+			type: String,
+			ref: 'Order'
+		},
+	],
+});
 
-    this.orders.push(selectedOrder)
+CustomerSchema.plugin(require("mongoose-autopopulate"));
 
-    return selectedOrder
-  }
-  static create({ id, name, address, orders, basket }) {
-    return new Customer(id, name, address, orders, basket)
-  }
-}
-
-module.exports = Customer
+module.exports = mongoose.model("Customer", CustomerSchema);
