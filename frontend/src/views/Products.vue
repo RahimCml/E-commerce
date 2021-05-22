4e1d7d7237f866248fe5bf9e01800c369ae208c3
RehimRay <rhmcml@gmail.ru>
2021-05-23 01:48:51 +0400

    Call fetchProducts in methods and assign fetchProducts to products

---
+++ frontend/src/views/Products.vue
@@ -0,0 +1,23 @@
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'Products',
+  data () {
+    return {
+      isLoading: true,
+      products: []
+    }
+  },
+  async created () {
+    this.products = await this.fetchProducts()
+  },
+  methods: {
+    ...mapActions(['fetchProducts'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 products
+</template>
