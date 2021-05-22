09576ff3fac5d9a6dff2e5f9c9df91a13381d661
RehimRay <rhmcml@gmail.ru>
2021-05-23 01:51:30 +0400

    Call fetchCustomers in methods and assign fetchCustomers to customers

---
+++ frontend/src/views/Customers.vue
@@ -0,0 +1,23 @@
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'Customers',
+  data () {
+    return {
+      isLoading: true,
+      customers: []
+    }
+  },
+  async created () {
+    this.customers = await this.fetchCustomers()
+  },
+  methods: {
+    ...mapActions(['fetchCustomers'])
+  }
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 customers
+</template>
