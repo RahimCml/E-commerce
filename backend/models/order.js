4d84514c7db196e15842e14f654870274e564827
RahimCml <rhmcml@mail.ru>
2021-06-20 02:05:48 +0400

    Convert order class to a mongoose model

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-	product: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Product",
-		autopopulate: { maxDepth: 1 },
-	},
-	seller: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Seller",
-		autopopulate: { maxDepth: 1 },
-	},
-	customer: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "Customer",
-		autopopulate: { maxDepth: 1 },
-	},
-	address: String,
-	totalPrice: String,
-	date: String,
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    autopopulate: { maxDepth: 1 },
+  },
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Seller',
+    autopopulate: { maxDepth: 1 },
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    autopopulate: { maxDepth: 1 },
+  },
+  address: String,
+  totalPrice: String,
+  date: String,
 });
 
-OrderSchema.plugin(require("mongoose-autopopulate"));
+OrderSchema.plugin(require('mongoose-autopopulate'));
 
-module.exports = mongoose.model("Order", OrderSchema);
+module.exports = mongoose.model('Order', OrderSchema);
